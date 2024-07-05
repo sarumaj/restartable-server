440f646bf88161405f94d34cf95fe22f03cf1558
Dawid Ciepiela <71898979-sarumaj@users.noreply.github.com>
2024-07-05 20:52:46 +0200

    use github.workspace

--- hooks/manual-auth-hook.sh
+++ hooks/manual-auth-hook.sh
@@ -4,3 +4,7 @@ echo "Please deploy a DNS TXT record under the name:"
 echo "_acme-challenge.$CERTBOT_DOMAIN."
 echo "with the following value:"
 echo "$CERTBOT_VALIDATION"
+
+# Output the DNS record to the GitHub Actions log for manual addition
+echo "::set-output name=dns_name::_acme-challenge.$CERTBOT_DOMAIN"
+echo "::set-output name=dns_value::$CERTBOT_VALIDATION"
